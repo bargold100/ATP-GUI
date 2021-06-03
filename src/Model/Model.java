17a6ea98f7628f5b4ef80bfd51dae5973a288684
meitii1212 <meitalgl@post.bgu.ac.il>
2021-06-03 15:14:43 +0300

    Initial commit

---
+++ src/Model/Model.java
@@ -0,0 +1,10 @@
+package Model;
+import ATPProjectJar.*;
+import algorithms.mazeGenerators.IMazeGenerator;
+import algorithms.mazeGenerators.MyMazeGenerator;
+
+public class Model {
+    IMazeGenerator mymaze = new MyMazeGenerator();
+
+
+}
