ef28b919c9bd28596bec0eb5d90d236a08466d4d
meitii1212 <meitalgl@post.bgu.ac.il>
2021-06-04 12:59:30 +0300

    updating IModel functions

--- src/Model/IModel.java
+++ src/Model/IModel.java
@@ -1,4 +1,22 @@
 package Model;
 
+import algorithms.search.Solution;
+
+import java.util.Observer;
+
 public interface IModel {
+
+    void generateMaze(int rows, int cols);
+    void updatePlayerLocation(Directions direction);
+    void solveMaze();
+
+    //observable
+    void assignObserver(Observer o);
+    //?? notify observers no need?
+
+    //GETTERS AND SETTERS
+    int[][] getMaze();
+    int getPlayerRow();
+    int getPlayerCol();
+    Solution getSolution();
 }
