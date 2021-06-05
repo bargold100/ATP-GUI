bfbc8cf602b4afca953085fa501a5b234607a1ed
meitii1212 <meitalgl@post.bgu.ac.il>
2021-06-05 20:32:45 +0300

    fixing java fx and trying to correct fxml

--- src/View/MazeDisplayer.java
+++ src/View/MazeDisplayer.java
@@ -1,18 +1,18 @@
-
 package View;
 
-        import algorithms.search.AState;
-        import javafx.beans.property.IntegerProperty;
-        import javafx.beans.property.SimpleStringProperty;
-        import javafx.beans.property.StringProperty;
-        import javafx.scene.canvas.Canvas;
-        import javafx.scene.canvas.GraphicsContext;
-        import javafx.scene.image.Image;
-        import javafx.scene.paint.Color;
+import algorithms.mazeGenerators.Position;
+import algorithms.search.AState;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 
-        import java.io.FileInputStream;
-        import java.io.FileNotFoundException;
-        import java.util.ArrayList;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class MazeDisplayer extends Canvas {
     private int[][] maze;
@@ -20,6 +20,8 @@ public class MazeDisplayer extends Canvas {
     private ArrayList<AState> solution;
     private int playerRow = 0;
     private int playerCol = 0;
+    private int TargetRow = 0;
+    private int TargetCol = 0;
     // wall and player images:
     StringProperty imageFileNameWall = new SimpleStringProperty();
     StringProperty imageFileNamePlayer = new SimpleStringProperty();
@@ -37,11 +39,16 @@ public class MazeDisplayer extends Canvas {
         return playerCol;
     }
 
+    public int getTargetRow(){return TargetRow;}
+
+    public int getTargetCol(){return TargetCol;}
+
     public void setPlayerPosition(int row, int col) {
         this.playerRow = row;
         this.playerCol = col;
         draw();
     }
+
 
     public String getImageFileNameWall() {
         return imageFileNameWall.get();
@@ -94,6 +101,12 @@ public class MazeDisplayer extends Canvas {
         this.maze = maze;
         draw();
     }
+    public void drawTargetPosition(int row, int col) {
+        this.TargetRow = row;
+        this.TargetCol = col;
+        draw();
+    }
+
 
     private void draw() {
         if(maze != null){
@@ -111,6 +124,7 @@ public class MazeDisplayer extends Canvas {
 
             drawMazeWalls(graphicsContext, cellHeight, cellWidth, rows, cols);
             drawPlayer(graphicsContext, cellHeight, cellWidth);
+            drawTarget(graphicsContext,cellHeight, cellWidth);
         }
     }
 
@@ -155,4 +169,183 @@ public class MazeDisplayer extends Canvas {
         else
             graphicsContext.drawImage(playerImage, x, y, cellWidth, cellHeight);
     }
+
+    private void drawTarget(GraphicsContext graphicsContext, double cellHeight, double cellWidth) {
+        double x = getPlayerCol() * cellWidth;
+        double y = getTargetRow() * cellHeight;
+        graphicsContext.setFill(Color.GREEN);
+
+        Image playerImage = null;
+        try {
+            playerImage = new Image(new FileInputStream(getImageFileNameTarget()));
+        } catch (FileNotFoundException e) {
+            System.out.println("There is no player image file");
+        }
+        if(playerImage == null)
+            graphicsContext.fillRect(x, y, cellWidth, cellHeight);
+        else
+            graphicsContext.drawImage(playerImage, x, y, cellWidth, cellHeight);
+    }
 }
+
+
+
+
+//===========================================================================================
+//package View;
+//
+//        import algorithms.search.AState;
+//        import javafx.beans.property.IntegerProperty;
+//        import javafx.beans.property.SimpleStringProperty;
+//        import javafx.beans.property.StringProperty;
+//        import javafx.scene.canvas.Canvas;
+//        import javafx.scene.canvas.GraphicsContext;
+//        import javafx.scene.image.Image;
+//        import javafx.scene.paint.Color;
+//
+//        import java.io.FileInputStream;
+//        import java.io.FileNotFoundException;
+//        import java.util.ArrayList;
+//
+//public class MazeDisplayer extends Canvas {
+//    private int[][] maze;
+//    // player position:
+//    private ArrayList<AState> solution;
+//    private int playerRow = 0;
+//    private int playerCol = 0;
+//    // wall and player images:
+//    StringProperty imageFileNameWall = new SimpleStringProperty();
+//    StringProperty imageFileNamePlayer = new SimpleStringProperty();
+//    StringProperty imageFileNameTarget = new SimpleStringProperty();
+//    StringProperty imageFileNameBackRound = new SimpleStringProperty();
+//
+//
+//    public void setSolution(ArrayList<AState> sol){this.solution = sol;}
+//
+//    public int getPlayerRow() {
+//        return playerRow;
+//    }
+//
+//    public int getPlayerCol() {
+//        return playerCol;
+//    }
+//
+//    public void setPlayerPosition(int row, int col) {
+//        this.playerRow = row;
+//        this.playerCol = col;
+//        draw();
+//    }
+//
+//    public String getImageFileNameWall() {
+//        return imageFileNameWall.get();
+//    }
+//
+//    public String imageFileNameWallProperty() { return imageFileNameWall.get(); }
+//
+//    public void setImageFileNameWall(String imageFileNameWall) {
+//        this.imageFileNameWall.set(imageFileNameWall);
+//    }
+//
+//    public String getImageFileNamePlayer() {
+//        return imageFileNamePlayer.get();
+//    }
+//
+//    public String imageFileNamePlayerProperty() {
+//        return imageFileNamePlayer.get();
+//    }
+//
+//    public void setImageFileNamePlayer(String imageFileNamePlayer) {
+//        this.imageFileNamePlayer.set(imageFileNamePlayer);
+//    }
+//
+//    public String getImageFileNameTarget() {
+//        return imageFileNameTarget.get();
+//    }
+//
+//    public String imageFileNameTargetProperty() {
+//        return imageFileNameTarget.get();
+//    }
+//
+//    public void setImageFileNameTarget(String imageFileNameTarget) {
+//        this.imageFileNameTarget.set(imageFileNameTarget);
+//    }
+//
+//    public String getImageFileNameBackRound() {
+//        return imageFileNameBackRound.get();
+//    }
+//
+//    public String imageFileNameBackRoundProperty() {
+//        return imageFileNameBackRound.get();
+//    }
+//
+//    public void setImageFileNameBackRound(String imageFileNameBackRound) {
+//        this.imageFileNameBackRound.set(imageFileNameBackRound);
+//    }
+//
+//
+//    public void drawMaze(int[][] maze) {
+//        this.maze = maze;
+//        draw();
+//    }
+//
+//    private void draw() {
+//        if(maze != null){
+//            double canvasHeight = getHeight();
+//            double canvasWidth = getWidth();
+//            int rows = maze.length;
+//            int cols = maze[0].length;
+//
+//            double cellHeight = canvasHeight / rows;
+//            double cellWidth = canvasWidth / cols;
+//
+//            GraphicsContext graphicsContext = getGraphicsContext2D();
+//            //clear the canvas:
+//            graphicsContext.clearRect(0, 0, canvasWidth, canvasHeight);
+//
+//            drawMazeWalls(graphicsContext, cellHeight, cellWidth, rows, cols);
+//            drawPlayer(graphicsContext, cellHeight, cellWidth);
+//        }
+//    }
+//
+//    private void drawMazeWalls(GraphicsContext graphicsContext, double cellHeight, double cellWidth, int rows, int cols) {
+//        graphicsContext.setFill(Color.RED);
+//
+//        Image wallImage = null;
+//        try{
+//            wallImage = new Image(new FileInputStream(getImageFileNameWall()));
+//        } catch (FileNotFoundException e) {
+//            System.out.println("There is no wall image file");
+//        }
+//
+//        for (int i = 0; i < rows; i++) {
+//            for (int j = 0; j < cols; j++) {
+//                if(maze[i][j] == 1){
+//                    //if it is a wall:
+//                    double x = j * cellWidth;
+//                    double y = i * cellHeight;
+//                    if(wallImage == null)
+//                        graphicsContext.fillRect(x, y, cellWidth, cellHeight);
+//                    else
+//                        graphicsContext.drawImage(wallImage, x, y, cellWidth, cellHeight);
+//                }
+//            }
+//        }
+//    }
+//
+//    private void drawPlayer(GraphicsContext graphicsContext, double cellHeight, double cellWidth) {
+//        double x = getPlayerCol() * cellWidth;
+//        double y = getPlayerRow() * cellHeight;
+//        graphicsContext.setFill(Color.GREEN);
+//
+//        Image playerImage = null;
+//        try {
+//            playerImage = new Image(new FileInputStream(getImageFileNamePlayer()));
+//        } catch (FileNotFoundException e) {
+//            System.out.println("There is no player image file");
+//        }
+//        if(playerImage == null)
+//            graphicsContext.fillRect(x, y, cellWidth, cellHeight);
+//        else
+//            graphicsContext.drawImage(playerImage, x, y, cellWidth, cellHeight);
+//    }
+//}
--- src/View/MyViewController.java
+++ src/View/MyViewController.java
@@ -24,65 +24,69 @@ import java.util.ResourceBundle;
 
 public class MyViewController<StringProperty> extends AView implements Initializable, Observer {
 
-        private MyViewModel viewModel;
-        public TextField textField_mazeRows;
-        public TextField textField_mazeColumns;
-        public MazeDisplayer mazeDisplayer;
-        public Label playerRow;
-        public Label playerCol;
+    private MyViewModel viewModel;
+    public TextField textField_mazeRows;
+    public TextField textField_mazeColumns;
+    public MazeDisplayer mazeDisplayer;
+    public Label playerRow;
+    public Label playerCol;
+
 
     SimpleStringProperty updatePlayerRow =  new SimpleStringProperty();
     SimpleStringProperty updatePlayerCol = new SimpleStringProperty();
 
-        public String getUpdatePlayerRow() {
-            return updatePlayerRow.get();
-        }
+    public String getUpdatePlayerRow() {
+        return updatePlayerRow.get();
+    }
 
-        public void setUpdatePlayerRow(int updatePlayerRow) {
-            this.updatePlayerRow.set(updatePlayerRow + "");
-        }
+    public void setUpdatePlayerRow(int updatePlayerRow) {
+        this.updatePlayerRow.set(updatePlayerRow + "");
+    }
 
-        public String getUpdatePlayerCol() {
-            return updatePlayerCol.get();
-        }
+    public String getUpdatePlayerCol() {
+        return updatePlayerCol.get();
+    }
 
-        public void setUpdatePlayerCol(int updatePlayerCol) {
-            this.updatePlayerCol.set(updatePlayerCol + "");
-        }
+    public void setUpdatePlayerCol(int updatePlayerCol) {
+        this.updatePlayerCol.set(updatePlayerCol + "");
+    }
 
-        @Override
-        public void initialize(URL url, ResourceBundle resourceBundle) {
-            playerRow.textProperty().bind((ObservableValue<? extends String>) updatePlayerRow);
-            playerCol.textProperty().bind((ObservableValue<? extends String>) updatePlayerCol);
-        }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        playerRow.textProperty().bind((ObservableValue<? extends String>) updatePlayerRow);
+        playerCol.textProperty().bind((ObservableValue<? extends String>) updatePlayerCol);
+    }
 
-        public void generateMaze(ActionEvent actionEvent) {
-            int rows = Integer.valueOf(textField_mazeRows.getText());
-            int cols = Integer.valueOf(textField_mazeColumns.getText());
+    public void generateMaze(ActionEvent actionEvent) {
+        int rows = Integer.valueOf(textField_mazeRows.getText());
+        int cols = Integer.valueOf(textField_mazeColumns.getText());
 
-            viewModel.generateMaze(rows, cols);
-        }
+        viewModel.generateMaze(rows, cols);
+        mazeDisplayer.drawMaze(viewModel.getMaze());
+        mazeDisplayer.drawTargetPosition(viewModel.getGoalRow(),viewModel.getGoalCol());
 
-        public void solveMaze(ActionEvent actionEvent) {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setContentText("Solving maze...");
-            alert.show();
-            viewModel.solveMaze();
-        }
+    }
 
-        public void openFile(ActionEvent actionEvent) {
-            FileChooser fc = new FileChooser();
-            fc.setTitle("Open maze");
-            fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Maze files (*.maze)", "*.maze"));
-            fc.setInitialDirectory(new File("./resources"));
-            File chosen = fc.showOpenDialog(null);
-            //...
-        }
+    public void solveMaze(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setContentText("Solving maze...");
+        alert.show();
+        viewModel.solveMaze();
+    }
 
-        public void keyPressed(KeyEvent keyEvent) {
-            viewModel.movePlayer(keyEvent);
-            keyEvent.consume();
-        }
+    public void openFile(ActionEvent actionEvent) {
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Open maze");
+        fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Maze files (*.maze)", "*.maze"));
+        fc.setInitialDirectory(new File("./resources"));
+        File chosen = fc.showOpenDialog(null);
+        //...
+    }
+
+    public void keyPressed(KeyEvent keyEvent) {
+        viewModel.movePlayer(keyEvent);
+        keyEvent.consume();
+    }
 
     public void setPlayerPosition(int row, int col){
         mazeDisplayer.setPlayerPosition(row, col);
@@ -90,38 +94,168 @@ public class MyViewController<StringProperty> extends AView implements Initializ
         setUpdatePlayerCol(col);
     }
 
-        //public void mouseClicked(MouseEvent mouseEvent) {
-            //mazeDisplayer.requestFocus();
-        //}
+    public void mouseClicked(MouseEvent mouseEvent) {
+    mazeDisplayer.requestFocus();
+    }
 
-        @Override
-        public void update(Observable o, Object arg) {
-            String change = (String) arg;
-            switch (change){
-                case "maze generated" -> mazeGenerated();
-                case "player moved" -> playerMoved();
-                case "maze solved" -> {
-                    try {
-                        mazeSolved();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+    @Override
+    public void update(Observable o, Object arg) {
+        String change = (String) arg;
+        switch (change){
+            case "maze generated" -> mazeGenerated();
+            case "player moved" -> playerMoved();
+            case "maze solved" -> {
+                try {
+                    mazeSolved();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                default -> System.out.println("Not implemented change: " + change);
             }
+            default -> System.out.println("Not implemented change: " + change);
         }
+    }
 
-        private void mazeSolved() throws Exception {
-            mazeDisplayer.setSolution(viewModel.getSolution());
-        }
+    private void mazeSolved() throws Exception {
+        mazeDisplayer.setSolution(viewModel.getSolution());
+    }
 
-        private void playerMoved() {
-            setPlayerPosition(viewModel.getPlayerRow(), viewModel.getPlayerCol());
-        }
+    private void playerMoved() {
+        setPlayerPosition(viewModel.getPlayerRow(), viewModel.getPlayerCol());
+    }
 
-        private void mazeGenerated() {
-            mazeDisplayer.drawMaze(viewModel.getMaze());
-        }
+    private void mazeGenerated() {
+        mazeDisplayer.drawMaze(viewModel.getMaze());
+    }
 
 
 }
+
+
+//=================================================================================
+//package View;
+//
+//import ViewModel.MyViewModel;
+//import algorithms.mazeGenerators.Maze;
+//import javafx.beans.property.SimpleStringProperty;
+//import javafx.beans.property.StringProperty;
+//import javafx.beans.value.ObservableValue;
+//import javafx.event.ActionEvent;
+//import javafx.fxml.Initializable;
+//import javafx.scene.control.Alert;
+//import javafx.scene.control.Label;
+//import javafx.scene.control.TextField;
+//import javafx.scene.input.KeyEvent;
+//import javafx.scene.input.MouseEvent;
+//import javafx.stage.FileChooser;
+//
+//
+//import java.awt.*;
+//import java.io.File;
+//import java.net.URL;
+//import java.util.Observable;
+//import java.util.Observer;
+//import java.util.ResourceBundle;
+//
+//public class MyViewController<StringProperty> extends AView implements Initializable, Observer {
+//
+//        private MyViewModel viewModel;
+//        public TextField textField_mazeRows;
+//        public TextField textField_mazeColumns;
+//        public MazeDisplayer mazeDisplayer;
+//        public Label playerRow;
+//        public Label playerCol;
+//
+//    SimpleStringProperty updatePlayerRow =  new SimpleStringProperty();
+//    SimpleStringProperty updatePlayerCol = new SimpleStringProperty();
+//
+//        public String getUpdatePlayerRow() {
+//            return updatePlayerRow.get();
+//        }
+//
+//        public void setUpdatePlayerRow(int updatePlayerRow) {
+//            this.updatePlayerRow.set(updatePlayerRow + "");
+//        }
+//
+//        public String getUpdatePlayerCol() {
+//            return updatePlayerCol.get();
+//        }
+//
+//        public void setUpdatePlayerCol(int updatePlayerCol) {
+//            this.updatePlayerCol.set(updatePlayerCol + "");
+//        }
+//
+//        @Override
+//        public void initialize(URL url, ResourceBundle resourceBundle) {
+//            playerRow.textProperty().bind((ObservableValue<? extends String>) updatePlayerRow);
+//            playerCol.textProperty().bind((ObservableValue<? extends String>) updatePlayerCol);
+//        }
+//
+//        public void generateMaze(ActionEvent actionEvent) {
+//            int rows = Integer.valueOf(textField_mazeRows.getText());
+//            int cols = Integer.valueOf(textField_mazeColumns.getText());
+//
+//            viewModel.generateMaze(rows, cols);
+//        }
+//
+//        public void solveMaze(ActionEvent actionEvent) {
+//            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+//            alert.setContentText("Solving maze...");
+//            alert.show();
+//            viewModel.solveMaze();
+//        }
+//
+//        public void openFile(ActionEvent actionEvent) {
+//            FileChooser fc = new FileChooser();
+//            fc.setTitle("Open maze");
+//            fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Maze files (*.maze)", "*.maze"));
+//            fc.setInitialDirectory(new File("./resources"));
+//            File chosen = fc.showOpenDialog(null);
+//            //...
+//        }
+//
+//        public void keyPressed(KeyEvent keyEvent) {
+//            viewModel.movePlayer(keyEvent);
+//            keyEvent.consume();
+//        }
+//
+//    public void setPlayerPosition(int row, int col){
+//        mazeDisplayer.setPlayerPosition(row, col);
+//        setUpdatePlayerRow(row);
+//        setUpdatePlayerCol(col);
+//    }
+//
+//        //public void mouseClicked(MouseEvent mouseEvent) {
+//            //mazeDisplayer.requestFocus();
+//        //}
+//
+//        @Override
+//        public void update(Observable o, Object arg) {
+//            String change = (String) arg;
+//            switch (change){
+//                case "maze generated" -> mazeGenerated();
+//                case "player moved" -> playerMoved();
+//                case "maze solved" -> {
+//                    try {
+//                        mazeSolved();
+//                    } catch (Exception e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//                default -> System.out.println("Not implemented change: " + change);
+//            }
+//        }
+//
+//        private void mazeSolved() throws Exception {
+//            mazeDisplayer.setSolution(viewModel.getSolution());
+//        }
+//
+//        private void playerMoved() {
+//            setPlayerPosition(viewModel.getPlayerRow(), viewModel.getPlayerCol());
+//        }
+//
+//        private void mazeGenerated() {
+//            mazeDisplayer.drawMaze(viewModel.getMaze());
+//        }
+//
+//
+//}
