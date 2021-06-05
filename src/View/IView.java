98f43ec98ea2525fd18188b5fd831a9566748ccc
meitii1212 <meitalgl@post.bgu.ac.il>
2021-06-05 11:47:08 +0300

    add display maze

--- src/View/IView.java
+++ src/View/IView.java
@@ -1,8 +1,13 @@
 package View;
 
+import algorithms.mazeGenerators.Maze;
+
 import java.io.IOException;
 
 public interface IView  {
     public void changeScene(String fxmlname) throws IOException;
     // add the set new view func ?
+
+    public void displayMaze(Maze maze);
+
 }
